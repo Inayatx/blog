067f07bf4833bd4ab2027dccd23b370eebb02d80
inayatx <inayatagaria@gmail.com>
2021-05-24 14:41:14 +0530

    add compose page and customise it

--- app.js
+++ app.js
@@ -5,38 +5,48 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-const aboutContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-const contactContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam faucibus purus in massa tempor.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio facilisis mauris sit amet.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home", {
-  startingContent: homeStartingContent,
+    startingContent: homeStartingContent,
   });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about", {
-  abtContent: aboutContent
+    abtContent: aboutContent
   });
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res) {
   res.render("contact", {
-  cnctContent: contactContent
+    cnctContent: contactContent
   });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  console.log(req.body.postTitle);
+  // console.log(req.body.postBody);
 });
 
 
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
