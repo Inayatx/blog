cc8596f5a7d6330c2442cc9a68009db0140e71fe
inayatx <inayatagaria@gmail.com>
2021-05-25 21:37:39 +0530

    add lodash for lower case

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// const lowerCase = require("lodash.lowercaseodemon");
+const _ = require("lodash");
 
 const homeStartingContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam faucibus purus in massa tempor.";
@@ -55,15 +57,15 @@ app.post("/compose", function(req, res) {
 });
 
 app.get("/posts/:postName", function(req, res) {
-  const requestedTitle = (req.params.postName);
+  const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post) {
-    const storedTitle = post.title;
+    const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
       console.log("Match found");
     } else {
-      console.log("Match not found");
+      console.log("Not a match");
     }
   });
 
